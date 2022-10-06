145ac457ec981d06eeb8cf29badb3af68f61dad4
kyunghoonkook <rnrrudgg68@gmail.com>
2022-10-06 16:10:40 +0900

    mini-blog-prac

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import MainPage from "./component/page/MainPage";
+import PostWritePage from "./component/page/PostWritePage";
+import PostViewPage from "./component/page/PostViewPage";
+const MainTitleText = styled.p`
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+`;
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <MainTitleText>경훈이 블로그</MainTitleText>
+      <Routes>
+        <Route index element={<MainPage />}></Route>
+        <Route path="post-write" element={<PostWritePage />}></Route>
+        <Route path="post/:postId" element={<PostViewPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
